c2b5c191a7c9705c17fb27db1b00dca4b406d838
nagato08 <tadjojeremie8@gmail.com>
2026-07-22 00:55:37 +0100

    hero acceuil modifie

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero({ lang, dict }: HeroProps) {
     <section className="relative min-h-[100svh] md:min-h-[92vh] flex items-end overflow-hidden border-b hairline">
       {/* Background image */}
       <Image
-        src="/images/hero_acceuil1.jpeg"
+        src="/images/hero-accueil-2026.jpeg"
         alt=""
         fill
         priority
